f93e68a75e28a1fc3751cfabeed89cd63f6b02e2
Shai Pe'er <shaip86@gmail.com>
2015-06-04 13:07:32 +0300

    DAL and BL added

---
+++ App_Code/EventerBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for EventerBL
+/// </summary>
+public class EventerBL
+{
+    EventerDAL dal;
+
+	public EventerBL()
+	{
+	    dal = new EventerDAL();	
+	}
+
+
+    public LinkedList<Event> getEventList()
+    {
+        return dal.getEventList();
+    }
+
+    public void addEvent(Event newEvent)
+    {
+        dal.addEvent(newEvent);
+    }
+
+
+
+
+}
---
+++ App_Code/EventerDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for EventerDAL
+/// </summary>
+public class EventerDAL
+{
+	public EventerDAL()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
