59d56facdee4e6c202143290fda1b89e5d52fb5f
urielsh <uriel.shany@gmail.com>
2015-06-16 18:39:51 +0300

    eventlist args validation
    yet not clear what do not work

--- EventList.aspx.cs
+++ EventList.aspx.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
 
 public partial class AddEvent : System.Web.UI.Page
 {
@@ -88,25 +89,24 @@ public partial class AddEvent : System.Web.UI.Page
 
         if(Event_Nav_CMD.Text.Equals("Save"))
         {
-            if(bl.updateEvent(ev))
+            if (isValid())
             {
-                Event_Name_TextBox.Text = "";
-                Type_TextBox.Text = "";
-                Number_Of_Guests_TextBox.Text = "";
-                Date_TextBox.Text = "";
-                Location_TextBox.Text = "";
-                Event_Nav_CMD.Text = "Add Event";
+                if (bl.updateEvent(ev))
+                {
+                    Event_Name_TextBox.Text = "";
+                    Type_TextBox.Text = "";
+                    Number_Of_Guests_TextBox.Text = "";
+                    Date_TextBox.Text = "";
+                    Location_TextBox.Text = "";
+                    Event_Nav_CMD.Text = "Add Event";
 
-
-                Page.Response.Redirect(HttpContext.Current.Request.Url.ToString(), true);
-
-                
+                    Page.Response.Redirect(HttpContext.Current.Request.Url.ToString(), true);
+                }
+                else
+                {
+                    Event_Nav_Eror_Label.Text = "*Error while updating event";
+                }
             }
-            else
-            {
-                Event_Nav_Eror_Label.Text = "*Error while updating event";
-            }
-            
         }
         else if (Event_Nav_CMD.Text.Equals("Add Event"))
         {
@@ -130,6 +130,24 @@ public partial class AddEvent : System.Web.UI.Page
 
     private void setSelectedIndex()
     {
-        selectedIndex = Convert.ToInt32(Event_list_GridView.SelectedRow.Cells[1].Text) - 1;
+            selectedIndex = Convert.ToInt32(Event_list_GridView.SelectedRow.Cells[1].Text) - 1;
+    }
+
+    private bool isValid() 
+    {
+        return !isNumerical(Event_Name_TextBox.Text) || isNumerical(Number_Of_Guests_TextBox.Text) || !isNumerical(Type_TextBox.Text) ||
+                 isValidDate(Date_TextBox.Text) || !isNumerical(Location_TextBox.Text);
+    }
+
+    private bool isValidDate(String date) 
+    {
+        DateTime dt;
+        var success = DateTime.TryParse(date, out dt);
+        return success;
+    }
+
+     private bool isNumerical(string input)
+    {
+        return new System.Text.RegularExpressions.Regex(@"^\d$").IsMatch(input);
     }
 }
